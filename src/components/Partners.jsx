3262b43e6176aaf05a800c2d94374485db29a1f7
akash02ab <akash02.ab@gmail.com>
2024-02-28 16:31:12 +0530

    feat: add partners component

---
+++ src/components/Partners.jsx
@@ -0,0 +1,59 @@
+import DriveLab from "../assets/images/drivelab.svg";
+import Quechua from "../assets/images/quechua.svg";
+import Unknown from "../assets/images/unknown.svg";
+import Nike from "../assets/images/nike.svg";
+import CocaCola from "../assets/images/cocacola.svg";
+import Puma from "../assets/images/puma.svg";
+import PartnersSection from "./style/partners";
+
+export default function Partners () {
+  const partners = [
+    {
+      name: "DriveLab",
+      logo: DriveLab,
+      link: "https://www.drivelab.io/",
+    },
+    {
+      name: "Quechua",
+      logo: Quechua,
+      link: "https://www.quechua.com/",
+    },
+    {
+      name: "Unknown",
+      logo: Unknown,
+      link: "#",
+    },
+    {
+      name: "Nike",
+      logo: Nike,
+      link: "https://www.nike.com/",
+    },
+    {
+      name: "CocaCola",
+      logo: CocaCola,
+      link: "https://www.coca-colacompany.com/nike",
+    },
+    {
+      name: "Puma",
+      logo: Puma,
+      link: "https://www.puma.com/",
+    }
+  ];
+  return (
+    <PartnersSection>
+      {
+        partners.map((partner) => (
+          <a
+            href={partner.link}
+            target="_blank"
+            rel="noreferrer"
+            className="partner"
+            key={partner.name}
+          >
+            <img src={partner.logo} alt={partner.name} />
+          </a>
+        ))
+      }
+    </PartnersSection>
+  )
+}
---
+++ src/components/style/partners.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const PartnersSection = styled.section`
+  max-width: 960px;
+  height: 60px;
+  margin: 0 auto;
+  padding: 200px 0; 
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 60px;
+`;
+
+export default PartnersSection;
