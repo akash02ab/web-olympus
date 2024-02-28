54e9e2a320d04fdbd08f4bb69a664170b0fc5e36
akash02ab <akash02.ab@gmail.com>
2024-02-28 11:00:09 +0530

    feat: add logo component

---
+++ src/components/style/logo.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const LogoStyle = styled.p`
+    color: var(--text-primary);
+    font-size: 25px;
+    font-weight: 600;
+    letter-spacing: 0.1ch;
+    line-height: 41px;
+
+    span:first-child {
+      margin-right: 10px;
+    }
+`;
+
+export default LogoStyle;
