852ff081eb4231ce65e47851a084024080099c35
akash02ab <akash02.ab@gmail.com>
2024-02-29 00:35:25 +0530

    style: add width + height to img

--- src/components/style/portfolioItem.js
+++ src/components/style/portfolioItem.js
@@ -7,6 +7,13 @@ const PortfolioItemWrapper = styled.article`
   display: flex;
   align-items: center;
   gap: 50px;
+  overflow-x: scroll;
+
+  img {
+    width: 430px;
+    height: 564px;
+    object-fit: cover;
+  }
 
   @media screen and (min-width: 1600px) {
     max-width: 100%;
