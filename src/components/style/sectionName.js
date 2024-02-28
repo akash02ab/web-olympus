5860606c65092f3b48d35bf91608aaa1111d42fb
akash02ab <akash02.ab@gmail.com>
2024-02-28 19:39:25 +0530

    feat: add resuable section name styled component

---
+++ src/components/style/sectionName.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SectionName = styled.h2`
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22.4px;
+  letter-spacing: 0.15ch;
+  text-transform: uppercase;
+  color: var(--text-secondary);
+`;
+
+export default SectionName;
