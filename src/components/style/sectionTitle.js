07e0963ce55af8e7fdef6b974262e1611fb92df0
akash02ab <akash02.ab@gmail.com>
2024-02-28 19:39:41 +0530

    feat: add resulable section title styled component

---
+++ src/components/style/sectionTitle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SectionTitle = styled.h3`
+  font-family: "Gamora";
+  font-size: 60px;
+  font-weight: 400;
+  line-height: 81px;
+  letter-spacing: 0.1ch;
+  color: var(--text-primary);
+`;
+
+export default SectionTitle;
