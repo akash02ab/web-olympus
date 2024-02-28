11a0d4a469b08748599854d723e066f6283435f2
akash02ab <akash02.ab@gmail.com>
2024-02-29 01:01:17 +0530

    feat: add testimonial component

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import Icon from "./Icon";
+import SectionName from "./style/sectionName";
+import SectionTitle from "./style/sectionTitle";
+import {
+  NavigationWrapper,
+  TestimonialItems,
+  TestimonialsSection,
+  UsersWrapper
+} from "./style/testimonials";
+import user1 from "../assets/images/user1.svg";
+import user2 from "../assets/images/user2.svg";
+
+export default function Testimonials () {
+  const [currentItem, setCurrentItem] = useState(0);
+  const users = [
+    {
+      name: "Chester Fell",
+      role: "CUSTOMER",
+      image: user1,
+    },
+    {
+      name: "Malina Williams",
+      role: "CUSTOMER",
+      image: user2,
+    }
+  ];
+  const testimonials = [
+    "1.But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.",
+    "2.But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.",
+    "3.But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete.",
+    "4.But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete."
+  ];
+  const ITEMS_TO_SHOW = 2;
+  const TOTAL_ITEMS = testimonials.length;
+  const LAST_TEIM = TOTAL_ITEMS - ITEMS_TO_SHOW;
+  const testimonalsToShow = testimonials.slice(currentItem, currentItem + ITEMS_TO_SHOW);
+
+  const prevClickHandler = () => {
+    if (currentItem > 0) {
+      setCurrentItem(currentItem - 1);
+    }
+  }
+
+  const nextClickHandler = () => {
+    if (currentItem < LAST_TEIM) {
+      setCurrentItem(currentItem + 1);
+    }
+  }
+
+  return (
+    <TestimonialsSection id="testimonials">
+      <div className="row">
+        <div>
+          <SectionName>TESTIMONIALS</SectionName>
+          <SectionTitle>Feedback From Our Clients</SectionTitle>
+        </div>
+        <NavigationWrapper>
+          <button onClick={prevClickHandler} disabled={currentItem === 0}>
+            <Icon name="left-arrow" />
+          </button>
+          <button onClick={nextClickHandler} disabled={currentItem === LAST_TEIM}>
+            <Icon name="right-arrow" />
+          </button>
+        </NavigationWrapper>
+      </div>
+      <TestimonialItems>
+        <Icon
+          name="semi-circle-revert"
+          className="semi-circle-revert"
+        />
+        {
+          testimonalsToShow.map((testimonial, index) => (
+            <div key={index} className="testimonial">
+              <p>"{testimonial}"</p>
+              <Icon
+                name="testimonial-decoration"
+                className="testimonial-decoration"
+              />
+            </div>
+          ))
+        }
+      </TestimonialItems>
+      <UsersWrapper>
+        {
+          users.map((user, index) => (
+            <div className="row">
+              <div key={index} className="user">
+                <img src={user.image} alt="avatar" className="avatar" />
+                <div className="user-info">
+                  <h3>{user.name}</h3>
+                  <span>{user.role}</span>
+                </div>
+              </div>
+              <Icon name="quote" />
+            </div>
+          ))
+        }
+      </UsersWrapper>
+    </TestimonialsSection>
+  );
+}
---
+++ src/components/style/testimonials.js
@@ -0,0 +1,161 @@
+import styled from "styled-components";
+
+const TestimonialsSection = styled.section`
+  margin-top: 200px;
+
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+`;
+
+const NavigationWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+
+  button {
+    width: 45px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background-color: var(--background-secondary);
+    outline: none;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 150ms ease-in;  
+
+    svg {
+      color: var(--text-primary);
+    }
+    
+    &:hover:enabled {
+      cursor: pointer;
+      background-color: #454545;
+      transition: all 150ms ease-in;
+
+      svg {
+        color: var(--brand-primary);
+        transition: all 150ms ease-in;
+      }
+    }
+
+    &:disabled {
+      cursor: default;
+      opacity: 0.5;
+    }
+  }
+`;
+
+const TestimonialItems = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+
+  .semi-circle-revert {
+    position: absolute;
+    left: -45px;
+    bottom: 10px;
+  }
+
+  .testimonial {
+    flex: 1;
+    width: fit-content;
+    max-width: 528px;
+    margin: 70px 28px 0 28px;
+    position: relative;
+    
+    &:before {
+      content: '';
+      height: 90px;
+      width: 4px;
+      background-color: var(--brand-primary);
+      position: absolute;
+      left: -28px;
+    }
+
+    p {
+      font-size: 20px;
+      font-weight: 400;
+      font-style: italic;
+      line-height: 32px;
+      letter-spacing: 0.05ch;
+      color: var(--text-primary);
+      margin-bottom: 28px;
+    }
+
+    .testimonial-decoration {
+      transform: translateX(-30px);
+    }
+  }
+`;
+
+const UsersWrapper = styled.div`
+  width: 100%;
+  margin-top: 62px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 72px;
+
+  .row {
+    flex: 1;
+    width: 100%;
+    min-width: 360px;
+    max-width: 528px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 72px;
+    
+    .user {
+      display: flex;
+      align-items: center;
+      gap: 24px;
+      
+      .avatar {
+        width: 64px;
+        aspect-ratio: 1;
+        border-radius: 50%;
+      }
+      
+      .user-info {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 2px;
+        
+        h3 {
+          font-size: 20px;
+          font-weight: 700;
+          line-height: 31px;
+          letter-spacing: 0.05ch;
+          color: var(--text-primary);
+        }
+
+        span {
+          font-size: 14px;
+          font-weight: 500;
+          line-height: 22.4px;
+          letter-spacing: 0.04ch;
+          text-transform: uppercase;
+          color: var(--brand-primary);
+        }
+      }
+    }
+  }
+
+`;
+
+export {
+  TestimonialsSection,
+  NavigationWrapper,
+  TestimonialItems,
+  UsersWrapper
+};
