960b1d9953dc057cde0fcec1fa7e0b9bb105e8e6
akash02ab <akash02.ab@gmail.com>
2024-02-29 15:09:17 +0530

    refactor: bind components into home component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import styled from 'styled-components';
-import About from './components/About';
-import Hero from './components/Hero';
 import Nav from './components/Nav';
-import Partners from './components/Partners';
-import Services from './components/Services';
-import Portfolio from './components/Portfolio';
-import PortfolioItem from './components/PortfolioItem';
-import Testimonials from './components/Testimonials';
+import Home from './components/Home';
 
 const AppWrapper = styled.div`
   min-height: 100vh;
   background-color: var(--background-primary);
 `;
 
-const SectionWrapper = styled.div`
-  max-width: 1440px;
-  margin: 0 auto;
-  padding: 0 135px;
-`;
-
 function App() {
   return (
     <AppWrapper>
       <Nav />
-      <Hero />
-      <SectionWrapper>
-        <Partners />
-        <About />
-        <Services />
-        <Portfolio />
-      </SectionWrapper>
-      <PortfolioItem />
-      <SectionWrapper>
-        <Testimonials />
-      </SectionWrapper>
+      <Home />
     </AppWrapper>
   );
 }
