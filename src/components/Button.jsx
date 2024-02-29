fd1b7ce3990547d95b2bfa4a76dc131885a61e03
akash02ab <akash02.ab@gmail.com>
2024-02-29 13:53:00 +0530

    fix: add onChange event

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import ButtonStyle from "./style/button";
 import buttonDecoration from "../assets/images/button-decoration.svg";
 
-export default function Button ({ children }) {
+export default function Button ({ children, onClick }) {
   return (
-    <ButtonStyle href="#">
+    <ButtonStyle href="#" onClick={onClick}>
       <img src={buttonDecoration} alt="" className="button-decoration" />
       <span>{children}</span>
       <img src={buttonDecoration} alt="" className="button-decoration" />
